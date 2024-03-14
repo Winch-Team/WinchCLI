d4df81b2e54ca1b674aca635bc6e192a3f6d3f39
Brody Critchlow <brodycritchlow@gmail.com>
2024-03-14 10:45:45 -0600

    Revert "feat(cli-parser): basic implementation of parser"
    This reverts commit 1675bea4dbb58d2b677d78d7ebaef95d09c56bb0.

--- winchcli/target/debug/deps/winchcli-0a9fd16443efa035.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Brody Critchlow\winchcli\target\debug\deps\libwinchcli-0a9fd16443efa035.rmeta: src\lib.rs
-
-C:\Users\Brody Critchlow\winchcli\target\debug\deps\winchcli-0a9fd16443efa035.d: src\lib.rs
-
-src\lib.rs:
