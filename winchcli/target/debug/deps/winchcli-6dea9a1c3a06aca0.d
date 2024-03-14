1675bea4dbb58d2b677d78d7ebaef95d09c56bb0
Brody Critchlow <brodycritchlow@gmail.com>
2024-03-14 10:44:30 -0600

    feat(cli-parser): basic implementation of parser

---
+++ winchcli/target/debug/deps/winchcli-6dea9a1c3a06aca0.d
@@ -0,0 +1,5 @@
+C:\Users\Brody Critchlow\winchcli\target\debug\deps\libwinchcli-6dea9a1c3a06aca0.rmeta: src\lib.rs
+
+C:\Users\Brody Critchlow\winchcli\target\debug\deps\winchcli-6dea9a1c3a06aca0.d: src\lib.rs
+
+src\lib.rs:
